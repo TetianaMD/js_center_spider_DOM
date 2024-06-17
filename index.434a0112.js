404b836728b6f44ee0c78da9f2f5c76b4b24f1ea
Tatiana <tanya_potapenko@bigmir.net>
2024-06-17 10:19:59 +0200

    make build

---
+++ index.434a0112.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),o=e.getBoundingClientRect(),c=o.width,l=o.height,i=t.offsetWidth,n=t.offsetHeight;t.style.left="".concat((c-i)/2,"px"),t.style.top="".concat((l-n)/2,"px");
+//# sourceMappingURL=index.434a0112.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const fieldRec = wall.getBoundingClientRect();
+const fieldWidth = fieldRec.width;
+const fieldHeight = fieldRec.height;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+const centerX = (fieldWidth - spiderWidth) / 2;
+const centerY = (fieldHeight - spiderHeight) / 2;
+spider.style.left = `${centerX}px`;
+spider.style.top = `${centerY}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
